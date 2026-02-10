04e2be4954b653498481e7aaf80265c6a104c5e5
Om Rokade <Omrokade904@gmail.com>
2026-02-10 16:52:43 +0530

    cors issue

--- src/index.js
+++ src/index.js
@@ -22,11 +22,7 @@ const app = express();
 /* Middleware*/
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",     // Vite dev
-      process.env.FRONTEND_URI // production frontend
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin:process.env.FRONTEND_URL,
     credentials: true
   })
 );
