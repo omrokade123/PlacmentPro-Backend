ea22551396dfd60fea3f5a30fb1b6637d67b98ee
Om Rokade <Omrokade904@gmail.com>
2026-02-10 16:37:21 +0530

    Change in cors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,16 @@ connectDB();
 const app = express();
 
 /* Middleware*/
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",     // Vite dev
+      process.env.FRONTEND_URI // production frontend
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 /* Routes */
